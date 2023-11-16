690e4bbd8915f974001b75701fcd8b268fc2d483
duttastu <148550607+duttastu@users.noreply.github.com>
2023-11-16 07:24:44 +0000

    new

---
+++ Objects/test.js
@@ -0,0 +1,14 @@
+ // objects
+const mySym ="myKey1"
+ const abc =   {
+    name :"Stuti",
+    age : 23,
+    email : "stuti123@gmail.com",
+    [mySym] : "mykey1" //symbol always write in objects
+ }
+
+ console.log(abc.name);
+ abc.name ="Stuti Dutta"
+ Object.freeze(abc)
+ console.log(abc);
+ console.log(abc[mySym]);
---
+++ array1/test.js
@@ -0,0 +1,4 @@
+const a = ["Stuti","bb","cc","dd"]
+const ab=["ee","ff","gg","hh"]
+console.log(a);
+console.log(ab);
