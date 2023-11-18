d6077a3e9d0c4ba2a243580ea22384888df908ee
duttastu <148550607+duttastu@users.noreply.github.com>
2023-11-18 08:52:55 +0000

    new

--- functions/scope.js
+++ functions/scope.js
@@ -1,16 +1,26 @@
-let a=200  //global scope
-if(true){
-    let a =10
-    const b=20
-    var c = 30
-    console.log("inner: ", a);
+// let a=200  //global scope
+// if(true){
+//     let a =10
+//     const b=20
+//     var c = 30
+//     console.log("inner: ", a);
 
-}// variables declared inside {} cannot be accessed from outside the block is called block scope
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
-console.log(a);
+// }// variables declared inside {} cannot be accessed from outside the block is called block scope
+ 
+// console.log(a);
 // console.log(typeof b);
 // console.log( c);
- 
+function one(){
+    const username ="stuti"
+
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+one()
