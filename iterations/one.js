bd068042be80919f369a2940a1d322a03e721b8f
duttastu <148550607+duttastu@users.noreply.github.com>
2023-11-21 13:02:41 +0000

    heyhey

---
+++ iterations/one.js
@@ -0,0 +1,44 @@
+// for
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element==5){
+        // console.log("5 is best number");
+    }
+    // console.log(element);
+    
+}
+
+for (let i = 0; i <=10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+   for (let j = 0; j <=10; j++) {
+    // console.log(`inner loop value: ${j}`);
+    // console.log(i + '*' + j + '=' +i*j);
+        
+    }
+    // const element = array[i];
+    
+}
+let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    // console.log(element);
+    
+}
+// break and continue
+
+// for (let i = 0; i < 10; i++) {
+//     if (i == 5){
+//         console.log(`detected 5`);
+//         break
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+for (let i = 0; i < 10; i++) {
+    if (i == 5){
+        console.log(`detected 5`);
+        continue
+    }
+    console.log(`value of i is ${i}`);
+}
