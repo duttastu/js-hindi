cbcdee6e3157b90df954673252355d8b13746c2e
duttastu <148550607+duttastu@users.noreply.github.com>
2023-11-21 13:16:49 +0000

    new

---
+++ iterations/dowhile.js
@@ -0,0 +1,23 @@
+
+// let i =0
+// while (i <=10) {
+//     // console.log(`value of i is ${i}`);
+//     // i++;
+//     i =i+3
+    
+// }
+// let myarra =["flash", "batman", "superman"]
+
+// let arr=0
+// while(arr < myarra.length){
+//     console.log(`value is ${myarra[arr]}`);
+//     arr++;
+// }
+
+let score =11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score<=10);
